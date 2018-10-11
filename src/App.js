60bd26200ec73cc4a696813e106bc5d02e8a4fa5
Marion <marion.touja@gmail.com>
2018-10-11 21:56:20 +0200

    React random quotes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
 import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Quote from "./Quote";
-import Lamp from "./Lamp";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      working: false
-    }  
-  }
-  handleClick = () => {
-    this.setState({working: !this.state.working});//a chaque click on modifie l'état.
-  };
   render() {
-    const homerWorking = this.state.working ? 'Homer is working' : 'Donuts break';
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/341817/homer_top-center.svg" className={this.state.working ? "App-logo-speed-up" : "App-logo"} alt="logo" />
-          <h1 className="App-title">'J'accepte de ne pas tout comprendre'</h1>
-        </header>
-        <p>{homerWorking}</p>
-          <button onClick={this.handleClick}><p>Homer{}</p></button>
-        <Quote
-          quote="I believe the children are the future... Unless we stop them now!"
-          character="Homer Simpson"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-        />
-        <Quote
-          quote="Me fail English? That's unpossible"
-          character="Ralph Wiggum"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-        />
-        <Lamp on />
-        <Lamp/>
+    return(
+      <div>
+        <Quote />
       </div>
     );
   }
 }
-
+  
 export default App;
--- src/Quote.js
+++ src/Quote.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import { Button } from 'reactstrap';
 
-const Quote = props => (
-  <figure>
-    <img src={props.image} alt={props.character} />
-    <figcaption>
-      <blockquote>{props.quote}</blockquote>
-      <cite>{props.character}</cite>
-    </figcaption>
-  </figure>
-);
+class Quote extends Component {
+  constructor() {
+    super();
+    this.state = {
+      character: "",
+      quote: "",
+      image:"",
+    }
+  }
+
+  getSimpsonQuote = (character, quote, image) => {
+    fetch(`https://thesimpsonsquoteapi.glitch.me/quotes`)
+      .then(response => response.json())
+      .then((data) => {
+          for(let i = 0; i < data.length; i++) {
+            this.setState({character: data[i].character , quote: data[i].quote , image: data[i].image,})
+          }
+      });
+  }
+
+  componentDidMount() {
+    this.getSimpsonQuote();
+  }
+
+  render() {
+    return(           
+    <div class="vertical-center">
+      <div class="card text-center element">
+        <img class="card-img-top"src={this.state.image} alt={this.state.describe}/>
+        <div class="card-body">
+          <h5 class="card-title">{this.state.character}</h5>
+          <p class="card-text">{this.state.quote}</p>
+          <Button outline color="success" onClick={this.getSimpsonQuote}>Enjoy !</Button>
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
 
 export default Quote;
